dceea617a5abb59d9be8f02ff1995a0f20911488
markopineli <markopineli@protonmail.com>
2026-02-27 13:53:36 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Guide to Activate TurboTax with License Code and Download TurboTax Software with License Code"
+html_title = "to Activate TurboTax Canada with License Code and Download TurboTax Premier with License Code Successfully"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
